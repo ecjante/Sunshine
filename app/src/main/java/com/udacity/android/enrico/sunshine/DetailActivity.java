dd2b2302c3d943e5af439bcfa6cd1e0357befc48
Enrico Jante <enrico.jante@gmail.com>
2018-01-26 14:22:00 -0800

    Complete S04.03

--- app/src/main/java/com/udacity/android/enrico/sunshine/DetailActivity.java
+++ app/src/main/java/com/udacity/android/enrico/sunshine/DetailActivity.java
@@ -1,13 +1,18 @@
 package com.udacity.android.enrico.sunshine;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mWeatherDetails;
 
     private TextView mDisplayWeatherTextview;
 
@@ -21,9 +26,24 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         if (intent != null) {
             if (intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String weatherData = intent.getStringExtra(Intent.EXTRA_TEXT);
-                mDisplayWeatherTextview.setText(weatherData);
+                mWeatherDetails = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mDisplayWeatherTextview.setText(mWeatherDetails);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        item.setIntent(createShareDetailsIntent());
+        return true;
+    }
+
+    private Intent createShareDetailsIntent() {
+        return ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mWeatherDetails)
+                .getIntent();
     }
 }
