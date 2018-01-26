abf12d77c3278b4b0956284a4e6a77da9f0954e6
Enrico Jante <enrico.jante@gmail.com>
2018-01-25 21:24:34 -0800

    Complete S01.01-02 & S02.01

--- app/src/main/java/com/udacity/android/enrico/sunshine/MainActivity.java
+++ app/src/main/java/com/udacity/android/enrico/sunshine/MainActivity.java
@@ -1,13 +1,66 @@
 package com.udacity.android.enrico.sunshine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.udacity.android.enrico.sunshine.data.SunshinePreferences;
+import com.udacity.android.enrico.sunshine.utilities.NetworkUtils;
+import com.udacity.android.enrico.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        mWeatherTextView = findViewById(R.id.tv_weather_data);
+
+        loadWeatherData();
+    }
+
+    public void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... strings) {
+            if (strings.length == 0)
+                return null;
+
+            String location = strings[0];
+            URL locationUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonResponse = NetworkUtils.getResponseFromHttpUrl(locationUrl);
+                String[] weatherResponse = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonResponse);
+
+                return weatherResponse;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+                for (String str : strings) {
+                    mWeatherTextView.append(str + "\n\n\n");
+                }
+            }
+        }
     }
 }
